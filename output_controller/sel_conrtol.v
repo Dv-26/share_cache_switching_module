c62077239e857546f405c98389726c9c4ea2ef31
Ban <2107109662@qq.com>
2024-05-12 08:46:43 +0000

    update output_controller/sel_conrtol.v.
    Signed-off-by: Ban <2107109662@qq.com>

--- output_controller/sel_conrtol.v
+++ output_controller/sel_conrtol.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ps
 
-`include "./generate_parameter.vh"
+`include "../generate_parameter.vh"
 
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
